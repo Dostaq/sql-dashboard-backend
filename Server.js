9e1e9d1b62e5ab081f227feaef2db64b22f20c2a
Dostaq Sharief <dostaqsharief@yahoo.com>
2025-02-26 06:45:36 +0000

    get login

--- Server.js
+++ Server.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const sql = require('mssql');
 require('dotenv').config();
 
+let users = [{ username: 'svc_dba_refresh', password: 'Temporal01.'}];
+
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -11,7 +13,12 @@ app.get('/', (req, res) => {
   res.send('SQL Web App Backend is Running!');
 });
 
+app.get('/login', (req, res) => {
+  res.send('Login endpoint is working, but use POST to Authenticate.');
+});
+
 app.post('/login', (req, res) => {
+  console.log('Login attempt:', req.body);
   const { username, password } = req.body;
   const user = users.find((u) => u.username === username && u.password === password);
   if (user) {
